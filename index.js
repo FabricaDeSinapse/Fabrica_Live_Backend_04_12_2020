d62ba6acd09d5a5d5efbd16dd902033e228e7a6f
Paulo Salvatore <salvatoregames@gmail.com>
2020-12-04 20:20:00 -0300

    Listagem de endpoints e criação dos endpoints de leitura (individual e todos)

--- index.js
+++ index.js
@@ -7,6 +7,36 @@ app.get('/hello', (req, res) => {
   res.send('Hello World');
 });
 
+/*
+Lista de Endpoints da aplicação CRUD de mensagens
+CRUD: Create, Read (Single & All), Update and Delete
+CRUD: Criar, Ler (Individual e Tudo), Atualizar e Remover
+- [GET] /mensagens - Retorna a lista de mensagens
+- [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+- [POST] /mensagens - Cria uma nova mensagem
+- [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+- [DELETE] /mensagens/{id} - Remover uma mensagem pelo ID
+*/
+
+const mensagens = [
+    "Essa é a primeira mensagem",
+    "Essa é a segunda mensagem"
+];
+
+// - [GET] /mensagens - Retorna a lista de mensagens
+app.get('/mensagens', (req, res) => {
+    res.send(mensagens);
+});
+
+// - [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+app.get('/mensagens/:id', (req, res) => {
+    const id = req.params.id - 1;
+
+    const mensagem = mensagens[id];
+
+    res.send(mensagem);
+});
+
 app.listen(port, () => {
     console.info(`App rodando em http://localhost:${port}`);
 });
