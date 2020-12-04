17e0a1a6122dda08ec659c5e448b715e135b4cb8
Paulo Salvatore <salvatoregames@gmail.com>
2020-12-04 20:37:13 -0300

    Criação do endpoint de remoção de mensagens e adaptação do endpoint de visualização das mensagens para ocultar informações 'null'

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const mensagens = [
 
 // - [GET] /mensagens - Retorna a lista de mensagens
 app.get('/mensagens', (req, res) => {
-    res.send(mensagens);
+    res.send(mensagens.filter(Boolean));
 });
 
 // - [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
@@ -59,6 +59,15 @@ app.put('/mensagens/:id', (req, res) => {
     mensagens[id] = mensagem;
 
     res.send(`Mensagem atualizada com sucesso: '${mensagem}'.`);
+});
+
+// - [DELETE] /mensagens/{id} - Remover uma mensagem pelo ID
+app.delete('/mensagens/:id', (req, res) => {
+    const id = req.params.id - 1;
+
+    delete mensagens[id];
+
+    res.send('Mensagem removida com sucesso.');
 });
 
 app.listen(port, () => {
